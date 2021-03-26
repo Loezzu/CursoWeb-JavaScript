cf6e765e5e22e7172d533b8096efa641a6d0253b
Luiz Souza <luixsouza12@outlook.com>
2021-03-26 02:10:33 -0300

    update fundamentos

--- fundamentos/organizacao.js
+++ fundamentos/organizacao.js
@@ -2,8 +2,8 @@ console.log("Sentença de código")
 
 {
   {
-        console.log("Olá");
-        console.log("Mundo!") // Padrão do curso
-	}
+    console.log("Olá");
+    console.log("Mundo!") // Padrão do curso
+  }
 
 }
